096dc13f2cca2563e325ef39173c27b768fd2fb9
João Tripa <jfptripa@gmail.com>
2024-06-03 23:40:21 +0100

    Add synchronization cycle with interval and add logging to file

--- sync-dir.py
+++ sync-dir.py
@@ -1,5 +1,9 @@
 import os
 import hashlib
+import time
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Dispatcher:
     def __init__(self, source):
@@ -9,39 +13,46 @@ class Dispatcher:
     def add_directory(self, directory):
         self.directories_list.append(directory)
 
-    def synchronize_directories(self):
-        for directory in self.directories_list:
-            self._compare_directories(self.source_directory,directory)
-            self._compare_files(self.source_directory,directory)
+    def synchronize_directories(self, sync_interval):
+        try:
+            while True:
+                self.source_directory.get_subdirectories_and_files()
+                for directory in self.directories_list:
+                    self._compare_directories(self.source_directory,directory)
+                    self._compare_files(self.source_directory,directory)
+                
+                time.sleep(int(sync_interval))
+        except KeyboardInterrupt:
+            pass
 
 
     def _compare_directories(self, source, replica):
         for subdir in source.subdirectories:
             if subdir not in replica.subdirectories:
-                print('Create subdirectory in replica - ' + subdir)
+                logger.info('[CREATE] Created new subdirectory in replica: ' + subdir)
                 replica.create_subdirectory(subdir)
 
         for subdir in replica.subdirectories:
             if subdir not in source.subdirectories:
-                print('Remove subdirectory in replica - ' + subdir)
+                logger.info('[DELETE] Removed subdirectory in replica: ' + subdir)
                 replica.remove_subdirectory(subdir)
     
     def _compare_files(self, source, replica):
         for file in source.files:
             if file not in replica.files:
-                print('Create file in replica - ' + file)
+                logger.info('[CREATE] Created new file in replica - ' + file)
                 replica.create_or_update_file(os.path.join(source.path,file),file)
 
         for file in replica.files:
             if file not in source.files:
-                print('Remove file in replica - ' + file)
+                logger.info('[DELETE] Removed file in replica - ' + file)
                 replica.delete_file(file)
                 continue
             
             if self._compare_files_hash(os.path.join(source.path,file),os.path.join(replica.path,file)):
-                print('File ' + file + ' is up to date.')
+                continue
             else:
-                print('Update file ' + file)
+                logger.info('[UPDATE] Updated file: ' + file)
                 replica.create_or_update_file(os.path.join(source.path,file),file)
                 
     def _compare_files_hash(self, source_file, file):
@@ -58,11 +69,12 @@ class Directory:
         self.path = path
         self.files = []
         self.subdirectories = []
-        self._get_subdirectories_and_files(self.path)
+        self.get_subdirectories_and_files()
     
     def create_or_update_file(self,source_file,file):
         os.system('cp '+ source_file + ' ' + os.path.join(self.path,file))
-        self.files.append(file)
+        if file not in self.files:
+            self.files.append(file)
     
     def delete_file(self,file):
         if os.path.exists(os.path.join(self.path,file)):
@@ -78,13 +90,29 @@ class Directory:
             os.system('rm -fr ' + os.path.join(self.path,subdirectory))
         self.subdirectories.remove(subdirectory)
 
-    def _get_subdirectories_and_files(self,root_path):
-        for path, subdirs, files in os.walk(root_path):
-            relative_path = os.path.relpath(path,root_path)
+    def get_subdirectories_and_files(self):
+        subdirectories = []
+        all_files = []
+
+        for path, subdirs, files in os.walk(self.path):
+            relative_path = os.path.relpath(path,self.path)
             for subdir in subdirs:
-                self.subdirectories.append(os.path.join(relative_path, subdir))
+                subdirectories.append(os.path.join(relative_path, subdir))
             for name in files:
-                self.files.append(os.path.join(relative_path, name))
+                all_files.append(os.path.join(relative_path, name))
+
+        self.subdirectories = subdirectories
+        self.files = all_files
+
+def configure_logging(log_file):
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    logger.setLevel(logging.DEBUG)
 
 if __name__ == "__main__":
     
@@ -105,10 +133,26 @@ if __name__ == "__main__":
             print('Can\'t synchronize directories without a replica directory.')
             exit()
 
+    sync_interval = input('Synchronization interval in seconds (eg. 300): ')
+
+    if not sync_interval.isdigit():
+        print('The synchronization interval you provided is not a digit.')
+        exit()
+    
+    log_file = input('Log file path: ')
+    if not os.path.isdir(os.path.dirname(log_file)):
+        print('Directory of log file ' + log_file + ' does not exist.')
+        exit()
+    elif os.path.isdir(log_file):
+        print('Log file path provided ' + log_file + ' is a directory.')
+        exit()
+
+    configure_logging(log_file)
+
     source_directory = Directory(source)
     replica_directory = Directory(replica)
 
     sync_dispatcher = Dispatcher(source_directory)
     sync_dispatcher.add_directory(replica_directory)
 
-    sync_dispatcher.synchronize_directories()
+    sync_dispatcher.synchronize_directories(sync_interval)
